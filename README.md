7118522c9e8c2aa51ba4e550fb16657a9a299ce0
Thomas Gilmore <tomtitans@aol.com>
2019-11-07 19:53:03 -0500

    refactor: Code to Add Class When Scroll to Section or Click The Section on the Navigation
    - Add description of project to README.md
    - Code to add class when the user scrolls or clicks the section on the navigation

---
+++ README.md
@@ -0,0 +1,3 @@
+Udacity Front End Developer Nanodegree Program - Project 2 - Landing Page
+
+This project requires us to build a multi-section landing page, with a dynamically updating navigational menu based on the amount of content that is added to the page.
--- js/app.js
+++ js/app.js
@@ -1,8 +1,14 @@
+/* Creating varibale nav to store the element with
+the id of navbar__list which is the ul element */
 const nav = document.querySelector('#navbar__list');
 
+//The text of the the new a elements that are being created
 var text = ["Section 1", "Section 2", "Section 3", "Section 4"];
+//The href link for the a elements that are being created
 var href = ["#section1", "#section2", "#section3", "#section4"];
+//The class of the a elements that are being created
 var newClass = ["section1", "section2", "section3", "section4"];
+//for loop to create all 4 li and a elements for the navigation 
 for(var i = 0; i <= 3; i++)
 {
     var a = document.createElement('a');
@@ -18,6 +24,8 @@ window.onscroll = function() {stickyNav()};
 
 var sticky = nav.offsetTop - 15;
 
+/* When the top of the window paces the top of the top of the
+navigation the navigation becomes sticky/fixed */
 function stickyNav() {
   if (window.pageYOffset >= sticky) {
     nav.classList.add('sticky');
@@ -26,65 +34,41 @@ function stickyNav() {
   }
 }
 
-function toggle1 () {
-  section1.classList.add('nav-click');
-  section2.classList.remove('nav-click');
-  section3.classList.remove('nav-click');
-  section4.classList.remove('nav-click');
-}
-
-function toggle2 () {
-  section2.classList.add('nav-click');
-  section1.classList.remove('nav-click');
-  section3.classList.remove('nav-click');
-  section4.classList.remove('nav-click');
-}
-
-function toggle3 () {
-  section3.classList.add('nav-click');
-  section1.classList.remove('nav-click');
-  section2.classList.remove('nav-click');
-  section4.classList.remove('nav-click');
-}
-
-function toggle4 () {
-  section4.classList.add('nav-click');
-  section1.classList.remove('nav-click');
-  section2.classList.remove('nav-click');
-  section3.classList.remove('nav-click');
-}
-
 var section1 = document.querySelector('.section1');
 var section2 = document.querySelector('.section2');
 var section3 = document.querySelector('.section3');
 var section4 = document.querySelector('.section4');
-section1.addEventListener('click', toggle1);
-section2.addEventListener('click', toggle2);
-section3.addEventListener('click', toggle3);
-section4.addEventListener('click', toggle4);
 
 const sections = document.querySelectorAll('.section');
 
+/* The function that runs when the user starts scrolling or clicks on a 
+navigation link that will add or remove the class of nav-click to change
+the style of that section numbers nav element. */
 function checkScroll(e) {
   sections.forEach(sections => {
     const activeClass = window.scrollY;
-    console.log(activeClass);
-    if (activeClass >= 188 && activeClass < 650) {
+
+    var section_1 = document.getElementById('section1');
+    var section_2 = document.getElementById('section2');
+    var section_3 = document.getElementById('section3');
+    var section_4 = document.getElementById('section4');
+
+    if (activeClass >= section_1.offsetTop-20 && activeClass < section_2.offsetTop-35) {
       section1.classList.add('nav-click');
       section2.classList.remove('nav-click');
       section3.classList.remove('nav-click');
       section4.classList.remove('nav-click');
-    } else if (activeClass >= 650 && activeClass < 1076) {
+    } else if (activeClass >= section_2.offsetTop-35 && activeClass < section_3.offsetTop-25) {
       section1.classList.remove('nav-click');
       section2.classList.add('nav-click');
       section3.classList.remove('nav-click');
       section4.classList.remove('nav-click');
-    } else if (activeClass >= 1076 && activeClass < 1576) {
+    } else if (activeClass >= section_3.offsetTop-35 && activeClass < section_4.offsetTop-35) {
       section1.classList.remove('nav-click');
       section2.classList.remove('nav-click');
       section3.classList.add('nav-click');
       section4.classList.remove('nav-click');
-    } else if (activeClass >= 1576) {
+    } else if (activeClass >= section_4.offsetTop-35) {
       section1.classList.remove('nav-click');
       section2.classList.remove('nav-click');
       section3.classList.remove('nav-click');
